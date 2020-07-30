3355c210e4ccb9a98b7326248afa3abff1a6b2a0
Edson Maciel <eddz-maciel@hotmail.com>
2020-07-30 15:35:38 -0700

    adding error handling

--- app.js
+++ app.js
@@ -1,19 +1,38 @@
 //
 const express = require('express');
-
 const app = express();
-//import routes
-
+//it show activity logs on your console
+const morgan = require('morgan');
+//import routes 
 const productRoutes = require('./api/routes/products');
+const ordersRoutes = require('./api/routes/orders');
 
+//implementing morgan in the app
+app.use(morgan('dev'));
+
+//doing the routes redirection to handle request
 app.use('/products', productRoutes);
+app.use('/orders', ordersRoutes);
 
-//work as a middleware
-// app.use((req, res, next) => {
-//     res.status(200).json({
-//         message: 'it works'
-//     });
-// });
+//handling errors
+app.use((req, res, next) => {
+    const error = new Error('not found');
+    error.status = 404;
+    //will forward this error request
+    next(error);
+});
+
+//this will handle all kinds of errors like db errors 
+app.use((error, req, res, next) => {
+    //here you pass the error status or 500 by default 
+    res.status(error.status || 500);
+    res.json({
+        error: {
+            status: error.status,
+            message: error.message
+        }
+    });
+});
 
 
 //
